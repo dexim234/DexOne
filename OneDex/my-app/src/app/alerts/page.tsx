625466662949786f9088eecfe3856bf8efd3e1a9
dexim234 <lantsov-contact.pro@mail.ru>
2026-05-22 00:12:21 +0300

    Add files via upload

---
+++ OneDex/my-app/src/app/alerts/page.tsx
@@ -0,0 +1,5 @@
+import ComingSoon from "@/components/ComingSoon";
+
+export default function AlertsPage() {
+  return <ComingSoon title="Alerts" />;
+}
---
+++ OneDex/my-app/src/app/market-hub/page.tsx
@@ -0,0 +1,5 @@
+import ComingSoon from "@/components/ComingSoon";
+
+export default function MarketHubPage() {
+  return <ComingSoon title="Market HUB" />;
+}
