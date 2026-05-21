6c350c4f55654eb3b72a61abbf277eb6f39f7493
dexim234 <lantsov-contact.pro@mail.ru>
2026-05-22 00:18:41 +0300

    Update next.config.ts

--- OneDex/my-app/next.config.ts
+++ OneDex/my-app/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'dist',
+  ...(process.env.EXPORT_STATIC === "true" && {
+    output: "export" as const,
+    distDir: "dist",
+  }),
 };
 
 export default nextConfig;
