d78afd20d4c94255e8e67763fdba0c06382d8915
dexim234 <lantsov-contact.pro@mail.ru>
2026-05-22 00:27:09 +0300

    Delete OneDex/my-app directory

--- OneDex/my-app/src/app/alerts/page.tsx
+++
@@ -1,5 +0,0 @@
-import ComingSoon from "@/components/ComingSoon";
-
-export default function AlertsPage() {
-  return <ComingSoon title="Alerts" />;
-}
--- OneDex/my-app/src/app/market-hub/page.tsx
+++
@@ -1,5 +0,0 @@
-import ComingSoon from "@/components/ComingSoon";
-
-export default function MarketHubPage() {
-  return <ComingSoon title="Market HUB" />;
-}
